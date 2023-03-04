59c50b09030183a2ec354cf5cf32a456a3385acd
crowdbotics-dev <engineering@crowdbotics.com>
2023-03-04 03:45:57 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ssdwfeqg
+admin.site.register(Ssdwfeqg)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,5 @@
+from django.conf import settings
 from django.db import models
+class Ssdwfeqg(models.Model):
+    'Generated Model'
+    fewqfeqw = models.BigIntegerField()
